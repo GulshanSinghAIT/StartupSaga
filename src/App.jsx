822b8a8bbb4555043e07541e4481d294fd836124
Gulshan <gulshansinghhhh@gmail.com>
2024-01-16 23:11:21 +0530

    added nav

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import React from "react"
+import Nav from "./Components/Nav"
+import Home from "./Components/Home"
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+
+function Landing() {
+  return (
+    <div>
+      <Nav />
+      <Home />
+    </div>
+  );
+}
+
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Landing />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App
